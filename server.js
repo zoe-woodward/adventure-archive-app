6bdd148de0b46c83b5d0f271219a42f55003f3da
zoe-woodward <zoewoodward94@gmail.com>
2024-10-28 19:18:10 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+
+const port = process.env.PORT ? process.env.PORT : "3000";
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan('dev'));
+
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+  });
+
+
+
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
